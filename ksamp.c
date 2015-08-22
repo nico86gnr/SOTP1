0cdeaaac171699b3ed9d862e1c4a11def98e0ff7
nico86gnr <nico86gnr@gmail.com>
2015-08-21 22:36:51 -0300

    punto B

--- ksamp.c
+++ ksamp.c
@@ -8,33 +8,102 @@
 #include <stdio.h>
 
 
-
-void cpuInfo(char path[])
+void cpuInfo()
 {
    FILE *fp;
-   char buff[255];
+   char buffer_tipo[255];
+   char buffer_modelo[255];
+   char *tipo=NULL;
+   char *modelo=NULL;
 
-   fp = fopen(path, "r");
+   fp = fopen("/proc/cpuinfo", "r");
 
-   printf("TIPO Y MODELO DE CPU: \n");
-   printf("--------------------- \n");
-
-   while(!feof(fp)){
-	   //copia hasta 24 caract. y añade uno nulo d fin d cadena
+   while(tipo==NULL){
+	   //copia hasta 254 caract. y añade uno nulo d fin d cadena
 	   //o copia hasta encontrar un cambio de linea
-	   fgets(buff, 255, (FILE*)fp);
-	   printf("%s", buff ); //imprime buff
-   }
-	   fclose(fp);
+	   fgets(buffer_tipo, 255, (FILE*)fp);
+
+	   tipo = strstr(buffer_tipo,"vendor_id");
+
+   }   fseek(fp, SEEK_SET, 0);
+
+   while(modelo==NULL){
+   	   //copia hasta 24 caract. y añade uno nulo d fin d cadena
+   	   //o copia hasta encontrar un cambio de linea
+   	   fgets(buffer_modelo, 255, (FILE*)fp);
+
+   	   modelo = strstr(buffer_modelo,"model name");
+
+      }
+
+   fclose(fp);
+
+   printf("%s\n", tipo);
+   printf("%s\n", modelo);
 
 }
 
 
+void versionKernel(){
+
+	FILE *fp;
+	char buffer_kernel[255];
+
+	fp = fopen("/proc/version", "r");
+	fgets(buffer_kernel, 255, (FILE*)fp);
+
+	fclose(fp);
+	printf("Version del Kernel: %s\n", buffer_kernel);
+
+}
+
+
+void tiempoSistema(){
+
+	FILE *fp;
+	float tiempo;
+	int dias;
+	int horas;
+	int minutos;
+	int segundos;
+
+	fp = fopen("/proc/uptime", "r");
+	fscanf(fp, "%f", &tiempo);
+
+	//printf("uptime: %f\n", tiempo);
+
+	tiempo=(tiempo/86400);
+	dias=(int)tiempo;
+
+	tiempo=tiempo-dias;
+	tiempo=tiempo*24;
+	horas=(int)tiempo;
+
+	tiempo=tiempo-horas;
+	tiempo=tiempo*60;
+	minutos=(int)tiempo;
+
+	tiempo=tiempo-minutos;
+	tiempo=tiempo*60;
+	segundos=(int)tiempo;
+
+	printf("tiempo: %d %s",dias,"dias   ");
+	printf("%d %s",horas,"horas   ");
+	printf("%d %s",minutos,"minutos   ");
+	printf("%d %s\n",segundos,"segundos   ");
+
+	fclose(fp);
+
+
+
+}
+
 
 int main(){
 
-	char path[]="/proc/cpuinfo";
-	cpuInfo(path);
+	cpuInfo();
+	versionKernel();
+	tiempoSistema();
 
 	return 0;
 }
