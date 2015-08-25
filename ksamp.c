87a9b85efe037dfea2877ec6c49cc8afc5345299
nico86gnr <nico86gnr@gmail.com>
2015-08-24 23:07:37 -0300

    funciones nroprocesos, cambioContexto, tiempoCPU

--- ksamp.c
+++ ksamp.c
@@ -53,7 +53,7 @@ void versionKernel(){
 	fgets(buffer_kernel, 255, (FILE*)fp);
 
 	fclose(fp);
-	printf("Version del Kernel: %s\n", buffer_kernel);
+	printf("Kernel: %s\n", buffer_kernel);
 
 }
 
@@ -87,7 +87,7 @@ void tiempoSistema(){
 	tiempo=tiempo*60;
 	segundos=(int)tiempo;
 
-	printf("tiempo: %d %s",dias,"dias   ");
+	printf("tiempo transcurrido del sistema: %d %s",dias,"dias   ");
 	printf("%d %s",horas,"horas   ");
 	printf("%d %s",minutos,"minutos   ");
 	printf("%d %s\n",segundos,"segundos   ");
@@ -101,28 +101,61 @@ void tiempoCPU(){
 
 	FILE *fp;
 	char cpu[10];
+	//char cpub[255];
 	int tiempo_usuario1;
 	int tiempo_usuario2;
 	int tiempo_sistema;
-	long tiempo_idle;
+	int tiempo_idle;
 
 	fp = fopen("/proc/stat", "r");
-	fscanf(fp,"%s""%d""%d""%d",cpu,&tiempo_usuario1,&tiempo_usuario2,
+	/*fgets(cpub, 255, (FILE*)fp);
+	fseek(fp, SEEK_SET, 0);*/
+
+	fscanf(fp,"%s""%d""%d""%d""%d",cpu,&tiempo_usuario1,&tiempo_usuario2,
 				&tiempo_sistema,&tiempo_idle);
-	printf(" %s",cpu);
-	printf("tiempo de uso de usuario: %d",tiempo_usuario1);
-	printf("tiempo de uso de usuario: %d",tiempo_usuario2);
-	printf("tiempo de uso de sistema: %d",tiempo_sistema);
-	printf("tiempo de proceso idle %d ", tiempo_idle);
+	//printf("%s\n",cpub);
+	//printf(" %s",cpu);
+	printf("tiempo de uso de usuario: %d\n",tiempo_usuario1);
+	printf("tiempo de uso de usuario con baja prioridad: %d\n",tiempo_usuario2);
+	printf("tiempo de uso de sistema: %d\n",tiempo_sistema);
+	printf("tiempo de uso de proceso idle %d\n",tiempo_idle);
 
 }
 void cambioContexto(){
 
+	FILE *fp;
+	char buffer[100];
+	char *cambio_contexto;
+
+	fp = fopen("/proc/stat", "r");
+
+	while(cambio_contexto==NULL){
+		fgets(buffer, 100, (FILE*)fp);
+		cambio_contexto=strstr(buffer,"ctxt");
+	}
+
+	printf("%s\n",cambio_contexto);
+
 }
+
 void inicioSistema(){
 
 }
+
 void nroProcesos(){
+
+	FILE *fp;
+	char buffer[100];
+	char *procesos;
+
+	fp = fopen("/proc/stat", "r");
+
+	while(procesos==NULL){
+		fgets(buffer, 100, (FILE*)fp);
+		procesos=strstr(buffer,"processes");
+	}
+
+	printf("%s\n",procesos);
 
 }
 
@@ -134,9 +167,11 @@ int main(int argc, char *argv[]){
 			cpuInfo();
 			versionKernel();
 			tiempoSistema();
-			tiempoCPU();
 		}
 	if(argv[1]=="-s"){
+			cpuInfo();
+			versionKernel();
+			tiempoSistema();
 			tiempoCPU();
 			cambioContexto();
 			inicioSistema();
